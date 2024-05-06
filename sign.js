8d985c4f0eacc1e7bb5d7d42ac6d65a0961cc4ee
whitewaffles <113029001+whitewaffles@users.noreply.github.com>
2024-05-06 18:57:19 +0900

    Update sign.js

--- sign.js
+++ sign.js
@@ -12,3 +12,37 @@ document.getElementById("togglePassword").addEventListener("click", function () 
         toggleIcon.classList.add("fa-solid", "fa-eye-slash");
     }
 });
+
+
+function login() {
+    const userid = document.getElementById('inputField').value;
+    const password = document.getElementById('passwordField').value;
+
+    // AJAX를 이용해 서버로 POST 요청 보내기
+    fetch('https://carnelian-abalone-periwinkle.glitch.me/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ userid: userid, password: password })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.loggedIn) {
+                alert('로그인이 되었습니다.');
+                window.location.href = "https://dlvm.netlify.app";
+                return;
+            } else {
+                // 인증 실패
+                alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
