81aff1ac8d3f89bef5fee72a3ac31c9ba1f71503
whitewaffles <113029001+whitewaffles@users.noreply.github.com>
2024-08-12 19:34:02 +0900

    Update login.js

--- login.js
+++ login.js
@@ -3,5 +3,6 @@ const REDIRECT_URI = 'https://dlvm.netlify.app/discord/profile';
 const DISCORD_ENDPOINT = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20email`;
 
 function handleLogin() {
+    console.log('Initiating Discord login...');
     window.location.href = DISCORD_ENDPOINT;
 }
